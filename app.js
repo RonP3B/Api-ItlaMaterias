725f1a40aeceb2c2826ad1fc360269aedd9ed065
Roniell Perez <roniell2016@hotmail.com>
2021-05-14 21:21:20 -0400

    changes

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const subjects = require("./routes/subjects");
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
+app.set("port", port);
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cors({ origin: "*" }));
 app.use("/",subjects);
 
-app.listen(port, () => console.log(`Running on ${port}`))
+app.listen(port, () => console.log(`Server running on ${port}...`))
