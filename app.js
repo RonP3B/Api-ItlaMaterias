53bce6a38a3bd9c3ec59352a7a18999646ac0a72
Roniell Perez <roniell2016@hotmail.com>
2021-05-14 20:28:06 -0400

    second commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const subjects = require("./routes/subjects");
+
+const app = express();
+const port = process.env.PORT || 5000
+
+app.use("/",subjects);
+
+app.listen(port, () => console.log(`Running on ${port}`))
--- src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const serverless = require("serverless-http")
-const subjects = require("./routes/subjects");
-
-const app = express();
-
-app.use("/.netlify/functions/app",subjects);
-
-module.exports.handler = serverless(app);
