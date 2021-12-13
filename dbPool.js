9a12366de33cf5bca35d0de317af553b28ad8d39
Roniell Perez <roniell2016@hotmail.com>
2021-12-12 20:38:54 -0400

    createConnection changed by createPool

---
+++ dbPool.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+const dbPool = mysql.createPool({
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+});
+
+module.exports = dbPool;
