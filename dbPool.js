d2cb2c1c629ed22e5b73dcbde02a46ca1a4296ca
Roniell Perez <roniell2016@hotmail.com>
2021-12-12 23:29:23 -0400

    adding env port

--- dbPool.js
+++ dbPool.js
@@ -6,6 +6,7 @@ const dbPool = mysql.createPool({
   database: process.env.DATABASE,
   user: process.env.USER,
   password: process.env.PASSWORD,
+  port: process.env.PORT,
 });
 
 module.exports = dbPool;
