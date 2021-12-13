8dcae26305893bf7cebf77a4f09c3b01e51f6011
Roniell Perez <roniell2016@hotmail.com>
2021-12-13 12:05:07 -0400

    port changed

--- dbPool.js
+++ dbPool.js
@@ -2,15 +2,10 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const dbPool = mysql.createPool({
-  connectionLimit: 1000,
-  connectTimeout: 60 * 60 * 1000,
-  acquireTimeout: 60 * 60 * 1000,
-  timeout: 60 * 60 * 1000,
   host: process.env.HOST,
   database: process.env.DATABASE,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  port: process.env.PORT,
 });
 
 module.exports = dbPool;
