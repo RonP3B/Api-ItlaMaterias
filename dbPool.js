1199a6efb3b219fe95c0dcab4bfb02e679a15498
Roniell Perez <roniell2016@hotmail.com>
2021-12-12 23:48:54 -0400

    changed by Ron

--- dbPool.js
+++ dbPool.js
@@ -1,38 +1,12 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-// const dbPool = mysql.createPool({
-//   host: process.env.HOST,
-//   database: process.env.DATABASE,
-//   user: process.env.USER,
-//   password: process.env.PASSWORD,
-//   port: process.env.PORT,
-// });
-
-let dbPool;
-
-const handleDisconnect = () => {
-  dbPool = mysql.createConnection({
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-  });
-
-  dbPool.connect((err) => {
-    if (err) {
-      console.log(err);
-      setTimeout(handleDisconnect, 2000);
-    } else console.log("Connection sucessful");
-  });
-
-  dbPool.on("error", (err) => {
-    console.log("db error", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") handleDisconnect();
-    else throw err;
-  });
-};
-
-handleDisconnect();
+const dbPool = mysql.createPool({
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
 
 module.exports = dbPool;
